a1786bdb936d435f9a3662d7b7f4708c74b24fee
mattusilveira <matheus12062007silveira7@gmail.com>
2025-05-30 15:43:35 -0300

    feat: inclusão do relacionamento de Livro com Categoria e Editora

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,3 +1,7 @@
+from django.db import models
+
+from .categoria import Categoria
+from .editora import Editora
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
@@ -7,3 +11,10 @@ class Livro(models.Model):
 
     def __str__(self):
         return f"({self.id}) {self.titulo} ({self.quantidade})"
+
+
+        categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros", null=True, blank=True
+    )
+
+        editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros", null=True, blank=True)
